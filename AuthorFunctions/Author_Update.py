d4b89365ab4ba5cea7a5e9ac5660613ebc3d508d
phung hoang <lnphunghoang190903@gmail.com>
2024-05-14 14:50:29 +0700

    Unit Test for Author to Update profile

---
+++ AuthorFunctions/Author_Update.py
@@ -0,0 +1,50 @@
+import unittest
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.edge.options import Options
+
+class Author_Update(unittest.TestCase):
+    def setUp(self):
+        self.debugger_address = 'localhost:8989'
+        self.c_options = Options()
+        self.c_options.add_experimental_option("debuggerAddress", "localhost:8989")
+        self.driver = webdriver.Edge(options=self.c_options)
+        self.driver.get('http://localhost/project/admin/update_profile.php')
+
+
+    def tearDown(self):
+        self.driver.quit()
+
+    def test_Update_Success(self):
+
+        username = self.driver.find_element(By.NAME, "name")
+        username.send_keys("admin101")
+
+        old_password = self.driver.find_element(By.NAME, "old_pass")
+        old_password.send_keys("admin102b")
+
+        new_password = self.driver.find_element(By.NAME, "new_pass")
+        new_password.send_keys("admin101")
+
+        con_password = self.driver.find_element(By.NAME, "confirm_pass")
+        con_password.send_keys("admin101")
+
+        self.driver.find_element(By.NAME, "submit").click()
+
+    def test_Update_Fail(self):
+        username = self.driver.find_element(By.NAME, "name")
+        username.send_keys("admin101")
+
+        old_password = self.driver.find_element(By.NAME, "old_pass")
+        old_password.send_keys("admin101")
+
+        new_password = self.driver.find_element(By.NAME, "new_pass")
+        new_password.send_keys("admin103")
+
+        con_password = self.driver.find_element(By.NAME, "confirm_pass")
+        con_password.send_keys("admin104")
+
+        self.driver.find_element(By.NAME, "submit").click()
+
+
